411771ee749db12ce636b29d72b7bacb2dd11ece
Ralph M <102826377+Rafagithub135@users.noreply.github.com>
2023-09-05 19:38:50 -0400

    created new project for school

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        System.out.println("What's up?");
+        System.out.println("I'm writing this at 2:31 PM.");
+        System.out.println("This IDE is NOT new to me.");
+        System.out.println("");
+        System.out.println("Above is an empty line.");
+    }
+}
