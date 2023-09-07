cf269940374b6c32508b2df610fea83cabe8e842
Ralph M <102826377+Rafagithub135@users.noreply.github.com>
2023-09-07 18:23:13 -0400

    completed first computer science assignment

---
+++ Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("**********");
+        System.out.println("*\\      ^*");
+        System.out.println("* \\    ^ *");
+        System.out.println("*  \\  ^  *");
+        System.out.println("*   \\^   *");
+        System.out.println("*   ^\\   *");
+        System.out.println("*  ^  \\  *");
+        System.out.println("* ^    \\ *");
+        System.out.println("*^      \\*");
+        System.out.println("**********");
+        System.out.println();
+        System.out.println();
+        System.out.println("    \"\"\"   ");
+        System.out.println("  \"     \"  ");
+        System.out.println(" \"       \" ");
+        System.out.println("\"         \"");
+        System.out.println(" \"       \" ");
+        System.out.println("  \"     \"  ");
+        System.out.println("    \"\"\"   ");
+        System.out.println();
+        System.out.println("          __     __");
+        System.out.println("         /  \\~~~/  \\");
+        System.out.println("   ,----(     ..    )");
+        System.out.println("  /      \\__     __/");
+        System.out.println(" /|         (\\  |(");
+        System.out.println("^ \\   /___\\  /\\ |");
+        System.out.println("   |__|   |__| \"");
+        System.out.println("                 credit: hjw '97");
+    }
+}
