3947328e280c9617155cc9e54ac85cc21ed6d092
Ralph M <102826377+Rafagithub135@users.noreply.github.com>
2023-09-11 16:39:49 -0400

    added an exercise from the book think java

---
+++ Exercise1.java
@@ -0,0 +1,13 @@
+public class Exercise1 {
+
+    public static void main(String[] args) {
+        System.out.println("1a). This is a Java statement.");
+        System.out.println("1b). A comment begins with 2 forward slashes and tells the programmer, or someone reading the code, a detail about that code.");
+        System.out.println("2). A program is portable if it can run on different operating systems.");
+        System.out.println("3). Compile means to translate a program from a high-level language to a low-level language.");
+        System.out.println("4a). An executable file is a file that can be run by the computer.");
+        System.out.println("4b). Executable is used as a noun because it is a file.");
+
+    }
+
+}
