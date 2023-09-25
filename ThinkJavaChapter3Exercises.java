b10f503d4354ebfac3e22d36d1a147e3aebb5203
Ralph M <102826377+Rafagithub135@users.noreply.github.com>
2023-09-25 15:10:28 -0400

    renamed exercise 1 and completed 3.1 of chapter 3 from Think Java Book

--- ThinkJavaChapter3Exercises.java
+++ ThinkJavaChapter3Exercises.java
@@ -3,6 +3,12 @@ import java.util.Scanner;
 public class ThinkJavaChapter3Exercises {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        
+        System.out.print("What is your name?");
+        String name = in.nextLine();
+        System.out.print("What is your age?");
+        int age = in.nextInt();
+        System.out.printf("Hello %s, age %d", name, age);
+        // If you try to enter a value with type int using %f and/or a value with type double using %d, Java will print an error message.  The types are mismatched.
+
     }
 }
