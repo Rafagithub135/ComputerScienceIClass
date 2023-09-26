796000332f1583c997dd82387839a8ed5d2bca4a
Ralph M <102826377+Rafagithub135@users.noreply.github.com>
2023-09-26 10:40:04 -0400

    reached stopping point at 4.7 - stacked diagrams in think java

--- ThinkJavaChapter4Exercises.java
+++ ThinkJavaChapter4Exercises.java
@@ -1,5 +1,33 @@
+import java.time.LocalTime;
+
 public class ThinkJavaChapter4Exercises {
     public static void main(String[] args) {
+        System.out.println("First line.");
+        threeLine();
+        System.out.println("Second line.");
+        printTime();
+    }
 
+    public static void newLine() {
+        System.out.println();
+    }
+
+    public static void threeLine() {
+        newLine();
+        newLine();
+        newLine();
+    }
+
+    public static void printTime() {
+        LocalTime currentTime = LocalTime.now();
+        int hour = currentTime.getHour();
+        int minute = currentTime.getMinute();
+        if (hour > 12) {
+            System.out.print(currentTime.getHour() - 12);
+        } else {
+            System.out.print(currentTime.getHour());
+        }
+        System.out.print(":");
+        System.out.println(minute);
     }
 }
