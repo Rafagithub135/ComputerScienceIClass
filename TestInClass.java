0ce53b3f7156b9cb0498e84038cd3e55ec776f0e
Ralph McCloskey <mccloskey.ralph35@gmail.com>
2023-10-03 17:18:57 -0400

    working on test class and added testinclass class for working while in class

---
+++ TestInClass.java
@@ -0,0 +1,9 @@
+public class TestInClass {
+    public static void main(String[] args) {
+        int num = 1;
+        while (num <= 100000) {
+            System.out.print(num + " ");
+            num++;
+        }
+    }
+}
