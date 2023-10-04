3b1ee358f8d36a40d80f7505cbaaaf1983840c54
Ralph M <102826377+Rafagithub135@users.noreply.github.com>
2023-10-04 13:43:24 -0400

    smashing bugs in bytetalker

--- TestInClass.java
+++ TestInClass.java
@@ -1,9 +1,24 @@
 public class TestInClass {
     public static void main(String[] args) {
-        int num = 1;
-        while (num <= 100000) {
-            System.out.print(num + " ");
-            num++;
+//        int num = 1;
+//        while (num <= 100000) {
+//            System.out.print(num + " ");
+//            num++;
+//        }
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 && i % 5) {
+                System.out.println("FizzBuzz");
+            }
+            else if (i % 3) {
+                System.out.println("Fizz");
+            }
+            else if (i % 5) {
+                System.out.println("Buzz");
+            }
+            else {
+                System.out.println(i);
+            }
+            }
         }
     }
 }
