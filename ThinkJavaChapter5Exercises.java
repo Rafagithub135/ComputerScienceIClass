9a094f50858f0a72df708f51a797a41bb641b87c
Ralph M <102826377+Rafagithub135@users.noreply.github.com>
2023-10-06 22:33:31 -0400

    started working on chapter 5 exercises

--- ThinkJavaChapter5Exercises.java
+++ ThinkJavaChapter5Exercises.java
@@ -1,23 +1,16 @@
 public class ThinkJavaChapter5Exercises {
     public static void main(String[] args) throws InterruptedException {
-        countdown(5);
-        System.out.println("This was only a test!  Do not be alarmed!");
-        displayBinary(7);
-    }
-    public static void countdown(int n) throws InterruptedException {
-        if (n == 0) {
-            System.out.println("Lift off!!!");
-        }
-        else {
-            System.out.println(n);
-            Thread.sleep(1000);
-            countdown(n - 1);
-        }
-    }
-    public static void displayBinary(int value) {
-        if (value > 0) {
-            displayBinary(value / 2);
-            System.out.print(value % 2);
+        x = null;
+        if (x > 0 && x < 10) {
+            System.out.println("positive single digit number.");
+        } else if (x > 10) {
+            System.out.println("positive double digit number.");
+        } else if (x < 0 && x > -10) {
+            System.out.println("negative single digit number.");
+        } else if (x < -10) {
+            System.out.println("negative double digit number.");
+        } else {
+            System.out.println("zero.");
         }
     }
 }
