77609b08ada18145a5977f981f70552dbdb6edb5
Ralph M <102826377+Rafagithub135@users.noreply.github.com>
2023-11-06 20:09:40 -0500

    working with assignment 7

--- Boxes.java
+++ Boxes.java
@@ -1,39 +1,19 @@
-import java.util.*;
-
 public class Boxes {
     public static void main(String[] args) {
         // Prints a rectangle 4 x 10 stars.
-        printStars();
+        printCharacters('*', 4, 10);
         System.out.println();
         //  Prints a rectangle 5 x 20 lines.
-        printLines();
+        printCharacters('|', 5, 20);
         System.out.println();
         // Prints a rectangle 7 x 6 a's.
-        printAs();
+        printCharacters('a', 7, 6);
     }
 
-    public static void printStars() {
-        for (int i = 0; i < 4; i++) {
-            for (int y = 0; y < 10; y++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-    }
-
-    public static void printLines() {
-        for (int i = 0; i < 5; i++) {
-            for (int y = 0; y < 20; y++) {
-                System.out.print("|");
-            }
-            System.out.println();
-        }
-    }
-
-    public static void printAs() {
-        for (int i = 0; i < 7; i++) {
-            for (int y = 0; y < 6; y++) {
-                System.out.print("a");
+    public static void printCharacters(char myChar, int numRows, int numChars) {
+        for (int i = 0; i < numRows; i++) {
+            for (int y = 0; y < numChars; y++) {
+                System.out.print(myChar);
             }
             System.out.println();
         }
