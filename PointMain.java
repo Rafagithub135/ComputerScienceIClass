4d5de07319bc822df042ca728f50e5bb7d54ad12
Ralph McCloskey <mccloskey.ralph35@gmail.com>
2023-11-28 18:24:32 -0500

    working on OOP

---
+++ PointMain.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+
+public class PointMain {
+    public static void main(String[] args) {
+        Point p1 = new Point();
+        p1.x = 7;
+        p1.y = 2;
+
+        Point p2 = new Point();
+        p2.x = 4;
+        p2.y = 3;
+    }
+}
+
+//
